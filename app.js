248addfa0ea75d2bfbf2bc10cdb78d941ca233c9
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-07-19 23:10:28 +0530

    Updated projects

--- app.js
+++ app.js
@@ -4,133 +4,152 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
 }
 
 var typed = new Typed("#changing-element", {
-    strings: ["Student", "Front-end developer", "Footballer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
-})
+  strings: ["Student", "Front-end developer", "Footballer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
 
-function opneGithubProfile(){
-    let githubProfile = "https://github.com/Akshansh029";
+function opneGithubProfile() {
+  let githubProfile = "https://github.com/Akshansh029";
 
-    window.open(githubProfile, "_blank");
+  window.open(githubProfile, "_blank");
 }
-function mailTo(){
-    let mailTo = "mailto:akshanshsingh00@gmail.com";
+function mailTo() {
+  let mailTo = "mailto:akshanshsingh00@gmail.com";
 
-    window.open(mailTo, "_blank");
+  window.open(mailTo, "_blank");
 }
-document.getElementById("brittany-site").addEventListener("click", event => {
-    window.open("https://v4.brittanychiang.com/", "_blank");
-})
+document.getElementById("brittany-site").addEventListener("click", (event) => {
+  window.open("https://v4.brittanychiang.com/", "_blank");
+});
 
 const navLinks = document.querySelectorAll(".nav-link");
 
-navLinks.forEach(navLink => {
-    navLink.addEventListener("click", event => {
-        event.preventDefault();
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", (event) => {
+    event.preventDefault();
 
-        const getID = event.currentTarget.getAttribute("href").substring(1);
-        const getElement = document.getElementById(getID);
+    const getID = event.currentTarget.getAttribute("href").substring(1);
+    const getElement = document.getElementById(getID);
 
-        getElement.scrollIntoView({
-            behavior: 'smooth'
-        })
-    })
+    getElement.scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
 
-document.getElementById("Epicurian-link").addEventListener("click", event => {
-    window.open("https://epicurian-restaurant.netlify.app/");
-})
-document.getElementById("Blog-link").addEventListener("click", event => {
-    window.open("https://ak-responsive-blog-site.netlify.app/");
-})
-document.getElementById("Weather-link").addEventListener("click", event => {
-    window.open("https://ak-weather-site.netlify.app/");
-})
-document.getElementById("Rock-paper-scissors-link").addEventListener("click", event => {
+document.getElementById("NoteNest-link").addEventListener("click", (event) => {
+  window.open("https://github.com/Akshansh029/NoteNest-app");
+});
+document.getElementById("Blog-link").addEventListener("click", (event) => {
+  window.open("https://ak-responsive-blog-site.netlify.app/");
+});
+document.getElementById("Weather-link").addEventListener("click", (event) => {
+  window.open("https://ak-weather-site.netlify.app/");
+});
+document
+  .getElementById("Rock-paper-scissors-link")
+  .addEventListener("click", (event) => {
     window.open("https://rock-paper-scissors-29.netlify.app/");
-})
-document.getElementById("Stopwatch-link").addEventListener("click", event => {
-    window.open("https://stopwatch-for-beginners.netlify.app/");
-})
-document.getElementById("Calculator-link").addEventListener("click", event => {
-    window.open("https://ak-simple-calculator.netlify.app/");
-})
-document.getElementById("roadmap.sh").addEventListener("click", event => {
-    window.open("https://roadmap.sh/full-stack");
-})
+  });
+document
+  .getElementById("VelocityMart-link")
+  .addEventListener("click", (event) => {
+    window.open("https://github.com/Rohan231203/Solvathon-VelocityMart");
+  });
+document.getElementById("ChatApp-link").addEventListener("click", (event) => {
+  window.open("https://github.com/Akshansh029/React-Firebase-Chat-Application");
+});
+document.getElementById("roadmap.sh").addEventListener("click", (event) => {
+  window.open("https://roadmap.sh/full-stack");
+});
 
 let projectItems = document.querySelectorAll(".project-item");
 
-projectItems.forEach(projectItem => {
-    let childElements = projectItem.children;
-    let h2Element = childElements[1];
+projectItems.forEach((projectItem) => {
+  let childElements = projectItem.children;
+  let h2Element = childElements[1];
 
-    projectItem.addEventListener('mouseover', event => {
-        h2Element.classList.add("heading-hover");
-    })
-    projectItem.addEventListener('mouseout', event => {
-        h2Element.classList.remove("heading-hover");
-    })
+  projectItem.addEventListener("mouseover", (event) => {
+    h2Element.classList.add("heading-hover");
+  });
+  projectItem.addEventListener("mouseout", (event) => {
+    h2Element.classList.remove("heading-hover");
+  });
 });
 
 function updateFixedElementPosition() {
-        const fixedElement = document.getElementById('media-links');
-        const viewportWidth = window.innerWidth;
-        const viewportHeight = window.innerHeight;
-        // console.log(window);
-        // console.log(viewportWidth);
-        // console.log(viewportHeight);
-        if((viewportHeight < 675) && (viewportWidth < 440)){
-            document.getElementById("about").style.marginTop = "20rem"
-        }
-        else if(viewportHeight < 560){
-                        document.getElementById("about").style.marginTop = "20rem";
-        }
-        else if(viewportHeight < 460){
-            document.getElementById("about").style.marginTop = "20rem";
-            document.getElementById("about").style.height = "fit-content";
-                        document.getElementById("footer").style.marginTop = "20rem";
-        }
-        else{
-            document.getElementById("about").style.marginTop = "5rem";
-            document.getElementById("footer").style.marginTop = "10rem";
-        }
+  const fixedElement = document.getElementById("media-links");
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  if (viewportHeight < 675 && viewportWidth < 440) {
+    document.getElementById("about").style.marginTop = "20rem";
+  } else if (viewportHeight < 560) {
+    document.getElementById("about").style.marginTop = "20rem";
+  } else if (viewportHeight < 460) {
+    document.getElementById("about").style.marginTop = "20rem";
+    document.getElementById("about").style.height = "fit-content";
+    document.getElementById("footer").style.marginTop = "20rem";
+  } else {
+    document.getElementById("about").style.marginTop = "5rem";
+    document.getElementById("footer").style.marginTop = "10rem";
+  }
 
-        if (viewportWidth < 769) {
-            fixedElement.style.position = 'relative';   
-            fixedElement.style.bottom = '80px';
-            fixedElement.style.left = '0px';
-            fixedElement.style.marginInline = 'auto';
-            fixedElement.style.marginBottom = '2rem';
-            fixedElement.children[0].style.flexDirection = "row";
-            document.getElementById('footer').appendChild(fixedElement);
-        } else {
-            fixedElement.style.position = 'fixed';
-            fixedElement.style.bottom = '0';
-            fixedElement.style.left = '50px';
-            fixedElement.style.marginBottom = '0';
+  if (viewportWidth < 769) {
+    fixedElement.style.position = "relative";
+    fixedElement.style.bottom = "80px";
+    fixedElement.style.left = "0px";
+    fixedElement.style.marginInline = "auto";
+    fixedElement.style.marginBottom = "2rem";
+    fixedElement.children[0].style.flexDirection = "row";
+    document.getElementById("footer").appendChild(fixedElement);
+  } else {
+    fixedElement.style.position = "fixed";
+    fixedElement.style.bottom = "0";
+    fixedElement.style.left = "50px";
+    fixedElement.style.marginBottom = "0";
 
-            fixedElement.children[0].style.flexDirection = "column";
-            document.body.appendChild(fixedElement);
-        }
-    }
+    fixedElement.children[0].style.flexDirection = "column";
+    document.body.appendChild(fixedElement);
+  }
+}
 
 updateFixedElementPosition();
 
-window.addEventListener('resize', updateFixedElementPosition);
+window.addEventListener("resize", updateFixedElementPosition);
+
+let sections = document.querySelectorAll("section");
+let header = document.querySelectorAll("header");
+let footer = document.querySelectorAll("footer");
+console.log(sections);
+console.log(header);
+console.log(footer);
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 200;
+    let height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+};
